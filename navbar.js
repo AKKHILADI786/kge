75d141a56a08a96a20057b14efe20a844ac6de35
Akshay kumar verma <69726264+AKKHILADI786@users.noreply.github.com>
2021-03-06 17:01:22 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,13 @@
+let toggle=document.getElementById("toggle")
+
+function myFunction() {
+    var x = document.getElementById("menuid");
+    if (x.style.display == "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
+
+
